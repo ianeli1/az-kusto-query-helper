bff4c612d24e2a67f2a9780f84c01761e0db9b8c
Ian <ianelicha@gmail.com>
2021-07-22 23:31:29 -0600

    update encryption

--- components/viewer.tsx
+++ components/viewer.tsx
@@ -20,6 +20,7 @@ export function Viewer({ query, pass, user, url, onClick }: ViewerProps) {
       server_url: url,
       server_password: pass,
       server_user: user,
+      trust: "TRUST_SYSTEM_CA_SIGNED_CERTIFICATES",
       initial_cypher: "match (n:Movie)-[d:REVIEWED]-(p:Person) return n,d,p",
       labels: {
         Person: {
